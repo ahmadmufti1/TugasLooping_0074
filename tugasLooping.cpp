4c38e381a6e8389fd4f87e91c4f1561388e88253
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-01 14:17:18 +0800

    membuat fungsi mengecek apakah bilangan tersebut adalah bilangan prima

--- tugasLooping.cpp
+++ tugasLooping.cpp
@@ -20,3 +20,18 @@ void input() {
     cout << "Masukkan angka: ";
     cin >> n;
 }
+
+// Membuat fungsi untuk mengecek bilangan prima
+bool cekPrima(int n){
+    if (n <= 1) 
+    return false;
+    
+    int i = 2;
+    while (i < n) {
+
+        if (n % i == 0 )
+        return false;
+        i++;
+    }
+    return true;
+}
