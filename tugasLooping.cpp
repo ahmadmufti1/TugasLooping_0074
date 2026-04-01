3923a7f2f200333bac027d9ca35af12d495e5fc2
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-01 14:14:49 +0800

    membuat fungsi untuk menampilkan menu

--- tugasLooping.cpp
+++ tugasLooping.cpp
@@ -4,3 +4,13 @@ using namespace std;
 // Deklarasi variabel global
 int n;
 int pilihan;
+
+// Membuat fungsi untuk menampilkan menu
+void menu() {
+    cout << "Pilih Menu" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Pilih menu: ";
+    cin >> pilihan;
+}
